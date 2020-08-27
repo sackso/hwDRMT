b5700161cec530d466f8e360429afea345eb84e8
SOKO <sackso@naver.com>
2020-08-27 21:28:12 +0900

    .

--- compile.bat
+++ compile.bat
@@ -3,5 +3,5 @@ javac -d . src\RMI\Hello\Server.java
 javac -d . src\RMI\Hello\Client.java
 
 rem cd \Project\workspace\hwDRMT
-rem start rmiregistry
-rem java RMI.Hello.Server
+rem start C:\jdk\java-1.8.0-openjdk-1.8.0.181-1\bin\rmiregistry
+rem C:\jdk\java-1.8.0-openjdk-1.8.0.181-1\bin\java com.hw.HWDRM2Server
